d3630ad80f08370470e1ff4eb89a9d883d8577fc
Ivan <mimeev91@gmail.com>
2022-09-23 00:42:22 +0300

    1.1.3

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,10 +1,11 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
-        UserServiceImpl userServiceImpl = new UserServiceImpl();
+        UserService userServiceImpl = new UserServiceImpl();
         userServiceImpl.createUsersTable();
         userServiceImpl.saveUser("Ivan", "Ivanov", (byte) 30);
         System.out.println("User с именем – Ivan добавлен в базу данных");
